74a574410c778661b511f2b49bee825714071186
belloibrahv <belloibrahv@gmail.com>
2025-01-20 16:09:13 +0100

    .

---
+++ src/config/phoneImages.ts
@@ -0,0 +1,52 @@
+export const PHONE_IMAGES = {
+  Google: [
+    {
+      name: 'Google Pixel 8a',
+      image: '/phones/pixel-8a.png'
+    },
+    {
+      name: 'Google Pixel 9 Pro',
+      image: '/phones/pixel-9-pro.png'
+    }
+  ],
+  Samsung: [
+    {
+      name: 'Samsung Galaxy S24 Ultra',
+      image: '/phones/s24-ultra.png'
+    },
+    {
+      name: 'Samsung Galaxy S24+',
+      image: '/phones/s24-plus.png'
+    }
+  ],
+  Xiaomi: [
+    {
+      name: 'Xiaomi 14 Ultra',
+      image: '/phones/xiaomi-14-ultra.png'
+    },
+    {
+      name: 'Xiaomi 14',
+      image: '/phones/xiaomi-14.png'
+    }
+  ],
+  OnePlus: [
+    {
+      name: 'OnePlus 12',
+      image: '/phones/oneplus-12.png'
+    },
+    {
+      name: 'OnePlus 12R',
+      image: '/phones/oneplus-12r.png'
+    }
+  ],
+  Motorola: [
+    {
+      name: 'Motorola Edge 40 Pro',
+      image: '/phones/edge-40-pro.png'
+    },
+    {
+      name: 'Motorola Edge 40',
+      image: '/phones/edge-40.png'
+    }
+  ]
+} as const;
--- src/utils/mockData.ts
+++ src/utils/mockData.ts
@@ -1,18 +1,27 @@
 import { Phone } from '../types/phone';
 import { faker } from '@faker-js/faker';
 import { FILTER_OPTIONS } from '../config/constants';
+import { PHONE_IMAGES } from '../config/phoneImages';
 
 export const generateMockPhones = (count: number): Phone[] => {
-  return Array.from({ length: count }, () => ({
-    id: faker.string.uuid(),
-    name: `${faker.company.name()} ${faker.number.int({ min: 1, max: 20 })}`,
-    brand: faker.helpers.arrayElement(FILTER_OPTIONS.brands),
-    price: faker.number.int({ min: 300, max: 1500 }),
-    primaryCamera: faker.helpers.arrayElement(FILTER_OPTIONS.primaryCamera),
-    features: faker.helpers.arrayElements(FILTER_OPTIONS.features, { min: 2, max: 5 }),
-    batteryLife: faker.helpers.arrayElement(FILTER_OPTIONS.batteryLife),
-    screenSize: faker.helpers.arrayElement(FILTER_OPTIONS.screenSizes),
-    imageUrl: `/api/placeholder/300/600`,
-    isNew: faker.datatype.boolean(),
-  }));
+  return Array.from({ length: count }, () => {
+    // First, select a brand
+    const brand = faker.helpers.arrayElement(FILTER_OPTIONS.brands);
+    
+    // Then, select a phone model from that brand's available models
+    const phoneModel = faker.helpers.arrayElement(PHONE_IMAGES[brand as keyof typeof PHONE_IMAGES]);
+
+    return {
+      id: faker.string.uuid(),
+      name: phoneModel.name,
+      brand,
+      price: faker.number.int({ min: 300, max: 1500 }),
+      primaryCamera: faker.helpers.arrayElement(FILTER_OPTIONS.primaryCamera),
+      features: faker.helpers.arrayElements(FILTER_OPTIONS.features, { min: 2, max: 5 }),
+      batteryLife: faker.helpers.arrayElement(FILTER_OPTIONS.batteryLife),
+      screenSize: faker.helpers.arrayElement(FILTER_OPTIONS.screenSizes),
+      imageUrl: phoneModel.image,
+      isNew: faker.datatype.boolean(),
+    };
+  });
 };
