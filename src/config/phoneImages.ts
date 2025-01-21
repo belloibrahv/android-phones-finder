c3fdd887d0ab5ffb4cb16e13152a66b281cdf125
uman95 <usman.aganiy@gmail.com>
2025-01-21 11:24:29 +0100

    added phone images

--- src/config/phoneImages.ts
+++ src/config/phoneImages.ts
@@ -13,15 +13,69 @@ import GooglePixel9ProXL from '../assets/images/GooglePixel9ProXL.webp';
 import GooglePixel9ProFold from '../assets/images/GooglePixel9ProFold.webp';
 import GooglePixelFold from '../assets/images/GooglePixelFold.webp';
 
+import Honor50 from '../assets/images/Honor50.webp';
+import HonorMagic4Pro from '../assets/images/HonorMagic4Pro.webp';
 
-import SamsungUtraS24 from '../assets/images/Samsung Galaxy S24 Ultra.jpg';
-import SamsungPlusS24 from '../assets/images/Samsung Galaxy S24 Plus.jpg';
-import Xiaomi14Ultra from '../assets/images/Xiaomi 14 Ultra.jpg';
-import Xiaomi14 from '../assets/images/Xiaomi Redmi Note 14 Pro Plus 5G.jpg';
-import OnePlus12 from '../assets/images/OnePlus 12.jpg';
-import OnePlus12R from '../assets/images/OnePlus 12R.jpg';
-import MotorolaEdge40Pro from '../assets/images/Motorola Edge 40 Pro.jpg';
-import MotorolaEdge40 from '../assets/images/Motorola Edge 40 Pro.jpg';
+import MotorolaRazr40Ultra from '../assets/images/MotorolaRazr40Ultra.webp';
+
+import NothingPhone1 from '../assets/images/NothingPhone1.webp';
+import NothingPhone2 from '../assets/images/NothingPhone2.webp';
+
+import OnePlus10Pro from '../assets/images/OnePlus10Pro.webp';
+import OnePlus10T5G from '../assets/images/OnePlus10T5G.webp';
+import OnePlus12 from '../assets/images/OnePlus12.webp';
+import OnePlus12R from '../assets/images/OnePlus12R.webp';
+import OnePlus115G from '../assets/images/OnePlus115G.webp';
+import OnePlusOpen from '../assets/images/OnePlusOpen.webp';
+
+import OppoFindN2Flip from '../assets/images/OppoFindN2Flip.webp';
+import OppoFindX5 from '../assets/images/OppoFindX5.webp';
+import OppoFindX5Lite from '../assets/images/OppoFindX5Lite.webp';
+import OppoFindX5Pro from '../assets/images/OppoFindX5Pro.webp';
+import OppoReno8 from '../assets/images/OppoReno8.webp';
+import OppoReno8Lite from '../assets/images/OppoReno8Lite.webp';
+import OppoReno8Pro from '../assets/images/OppoReno8Pro.webp';
+
+import SonyXperia1III from '../assets/images/SonyXperia1III.webp';
+import SonyXperia1IV from '../assets/images/SonyXperia1IV.webp';
+import SonyXperia5II from '../assets/images/SonyXperia5III.webp';
+import SonyXperia10III from '../assets/images/SonyXperia10III.webp';
+import SonyXperiaProI from '../assets/images/SonyXperiaProI.webp';
+
+import Xiaomi11T from '../assets/images/Xiaomi11T.webp';
+import Xiaomi11TPro from '../assets/images/Xiaomi11TPro.webp';
+import Xiaomi12 from '../assets/images/Xiaomi12.webp';
+import Xiaomi12Pro from '../assets/images/Xiaomi12Pro.webp';
+import Xiaomi13T from '../assets/images/Xiaomi13T.webp';
+import Xiaomi13TPro from '../assets/images/Xiaomi13TPro.webp';
+import Xiaomi14 from '../assets/images/Xiaomi14.webp';
+import Xiaomi14Ultra from '../assets/images/Xiaomi14Ultra.webp';
+import XiaomiRedmiNote11Pro5G from '../assets/images/XiaomiRedmiNote11Pro5G.webp';
+
+import SamsungGalaxyA335G from '../assets/images/SamsungGalaxyA335G.webp';
+import SamsungGalaxyA53 from '../assets/images/SamsungGalaxyA53.webp';
+import SamsungGalaxyA54 from '../assets/images/SamsungGalaxyA54.webp';
+import SamsungGalaxyFlip6 from '../assets/images/SamsungGalaxyFlip6.webp';
+import SamsungGalaxyFold6 from '../assets/images/SamsungGalaxyFold6.webp';
+import SamsungGalaxyM53 from '../assets/images/SamsungGalaxyM53.webp';
+import SamsungGalaxyS20FE from '../assets/images/SamsungGalaxyS20FE.webp';
+import SamsungGalaxyS21FE4G from '../assets/images/SamsungGalaxyS21FE4G.webp';
+import SamsungGalaxyS22plus from '../assets/images/SamsungGalaxyS22plus.webp';
+import SamsungGalaxyS22 from '../assets/images/SamsungGalaxyS22.webp';
+import SamsungGalaxyS22Ultra from '../assets/images/SamsungGalaxyS22Ultra.webp';
+import SamsungGalaxyS23plus from '../assets/images/SamsungGalaxyS23plus.webp';
+import SamsungGalaxyS23 from '../assets/images/SamsungGalaxyS23.webp';
+import SamsungGalaxyS23FE from '../assets/images/SamsungGalaxyS23FE.webp';
+import SamsungGalaxyS23Ultra from '../assets/images/SamsungGalaxyS23Ultra.webp';
+import SamsungGalaxyS24plus from '../assets/images/SamsungGalaxyS24plus.webp';
+import SamsungGalaxyS24 from '../assets/images/SamsungGalaxyS24.webp';
+import SamsungGalaxyS24Ultra from '../assets/images/SamsungGalaxyS24Ultra.webp';
+import SamsungGalaxyZFlip3 from '../assets/images/SamsungGalaxyZFlip3.webp';
+import SamsungGalaxyZFlip4 from '../assets/images/SamsungGalaxyZFlip4.webp';
+import SamsungGalaxyZFlip5 from '../assets/images/SamsungGalaxyZFlip5.webp';
+import SamsungGalaxyZFold3 from '../assets/images/SamsungGalaxyZFold3.webp';
+import SamsungGalaxyZFold4 from '../assets/images/SamsungGalaxyZFold4.webp';
+import SamsungGalaxyZFold5 from '../assets/images/SamsungGalaxyZFold5.webp';
 
 export const PHONE_IMAGES = {
   Google: [
@@ -82,44 +136,248 @@ export const PHONE_IMAGES = {
       image: GooglePixelFold,
     }
   ],
-  Samsung: [
+  Honor: [
     {
-      name: 'Samsung Galaxy S24 Ultra',
-      image: SamsungUtraS24
+      name: 'Honor 50',
+      image: Honor50
     },
     {
-      name: 'Samsung Galaxy S24+',
-      image: SamsungPlusS24,
+      name: 'Honor Magic 4 Pro',
+      image: HonorMagic4Pro,
     }
   ],
-  Xiaomi: [
+  Motorola : [
     {
-      name: 'Xiaomi 14 Ultra',
-      image: Xiaomi14Ultra,
+      name: 'Motorola Razr 40 Ultra',
+      image: MotorolaRazr40Ultra
+    }
+  ],
+  Nothing: [
+    {
+      name: 'Nothin Phone (1)',
+      image: NothingPhone1
     },
     {
-      name: 'Xiaomi 14',
-      image: Xiaomi14,
+      name: 'Nothing Phone (2)',
+      image: NothingPhone2,
     }
   ],
   OnePlus: [
     {
-      name: 'OnePlus 12',
-      image: OnePlus12, 
+      name: 'One Plus 10 Pro',
+      image: OnePlus10Pro
     },
     {
-      name: 'OnePlus 12R',
+      name: 'Pne Plus 10T 5G',
+      image: OnePlus10T5G
+    },
+    {
+      name: 'One Plus 11 5G',
+      image: OnePlus115G
+    },
+    {
+      name: 'One Plus 12',
+      image: OnePlus12
+    },
+    {
+      name: 'One Plus 12R',
       image: OnePlus12R
+    },
+    {
+      name: 'One Plus Open',
+      image: OnePlusOpen
     }
   ],
-  Motorola: [
+  Oppo: [
     {
-      name: 'Motorola Edge 40 Pro',
-      image: MotorolaEdge40Pro, 
+      name: 'Oppo Find N2 Flip',
+      image: OppoFindN2Flip
     },
     {
-      name: 'Motorola Edge 40',
-      image: MotorolaEdge40,
+      name: 'Oppo Find X5',
+      image: OppoFindX5
+    },
+    {
+      name: 'Oppo Find X5 Lite',
+      image: OppoFindX5Lite
+    },
+    {
+      name: 'Oppo Find X5 Pro',
+      image: OppoFindX5Pro
+    },
+    {
+      name: 'Oppo Reno 8',
+      image: OppoReno8
+    },
+    {
+      name: 'Oppo Reno 8 Lite',
+      image: OppoReno8Lite
+    },
+    {
+      name: 'Oppo Reno 8 Pro',
+      image: OppoReno8Pro
+    }
+  ],
+  Sony: [
+    {
+      name: 'Sony Xperia 10 III',
+      image: SonyXperia10III
+    },
+    {
+      name: 'Sony Xperia 1 III',
+      image: SonyXperia1III
+    },
+    {
+      name: 'Sony Xperia 1 IV',
+      image: SonyXperia1IV
+    },
+    {
+      name: 'Sony Xperia 5 II',
+      image: SonyXperia5II
+    },
+    {
+      name: 'Sony Xperia Pro-I',
+      image: SonyXperiaProI
+    }
+  ],
+  Samsung: [
+    {
+      name: 'Samsung Galaxy A33 5G',
+      image: SamsungGalaxyA335G
+    },
+    {
+      name: 'Samsung Galaxy A53',
+      image: SamsungGalaxyA53
+    },
+    {
+      name: 'Samsung Galaxy A54',
+      image: SamsungGalaxyA54
+    },
+    {
+      name: 'Samsung Galaxy Flip 6',
+      image: SamsungGalaxyFlip6
+    },
+    {
+      name: 'Samsung Galaxy Fold 6',
+      image: SamsungGalaxyFold6
+    },
+    {
+      name: 'Samsung Galaxy M53',
+      image: SamsungGalaxyM53
+    },
+    {
+      name: 'Samsung Galaxy S20 FE',
+      image: SamsungGalaxyS20FE
+    },
+    {
+      name: 'Samsung Galaxy S21 FE 4G',
+      image: SamsungGalaxyS21FE4G
+    },
+    {
+      name: 'Samsung Galaxy S22',
+      image: SamsungGalaxyS22
+    },
+    {
+      name: 'Samsung Galaxy S22 Ultra',
+      image: SamsungGalaxyS22Ultra
+    },
+    {
+      name: 'Samsung Galaxy S23',
+      image: SamsungGalaxyS23
+    },
+    {
+      name: 'Samsung Galaxy S23 FE',
+      image: SamsungGalaxyS23FE
+    },
+    {
+      name: 'Samsung Galaxy S23 Ultra',
+      image: SamsungGalaxyS23Ultra
+    },
+    {
+      name: 'Samsung Galaxy S24',
+      image: SamsungGalaxyS24
+    },
+    {
+      name: 'Samsung Galaxy S24 Ultra',
+      image: SamsungGalaxyS24Ultra
+    },
+    {
+      name: 'Samsung Galaxy Z Flip 3',
+      image: SamsungGalaxyZFlip3
+    },
+    {
+      name: 'Samsung Galaxy Z Flip 4',
+      image: SamsungGalaxyZFlip4
+    },
+    {
+      name: 'Samsung Galaxy Z Flip 5',
+      image: SamsungGalaxyZFlip5
+    },
+    {
+      name: 'Samsung Galaxy Z Fold 3',
+      image: SamsungGalaxyZFold3
+    },
+    {
+      name: 'Samsung Galaxy Z Fold 4',
+      image: SamsungGalaxyZFold4
+    },
+    {
+      name: 'Samsung Galaxy Z Fold 5',
+      image: SamsungGalaxyZFold5
+    },
+    {
+      name: 'Samsung Galaxy Z Fold 5',
+      image: SamsungGalaxyZFold5
+    },
+    {
+      name: 'Samsung Galaxy S22+',
+      image: SamsungGalaxyS22plus
+    },
+    {
+      name: 'Samsung Galaxy S23+',
+      image: SamsungGalaxyS23plus
+    },
+    {
+      name: 'Samsung Galaxy S24+',
+      image: SamsungGalaxyS24plus
+    }
+  ],
+  Xiaomi: [
+    {
+      name: 'Xiaomi 11T',
+      image: Xiaomi11T
+    },
+    {
+      name: 'Xiaomi 11T Pro',
+      image: Xiaomi11TPro
+    },
+    {
+      name: 'Xiaomi 12',
+      image: Xiaomi12
+    },
+    {
+      name: 'Xiaomi 12 Pro',
+      image: Xiaomi12Pro
+    },
+    {
+      name: 'Xiaomi 13T',
+      image: Xiaomi13T
+    },
+    {
+      name: 'Xiaomi 13T Pro',
+      image: Xiaomi13TPro
+    },
+    {
+      name: 'Xiaomi 14',
+      image: Xiaomi14
+    },
+    {
+      name: 'Xiaomi 14 Ultra',
+      image: Xiaomi14Ultra
+    },
+    {
+      name: 'Xiaomi Redmi Note 11 Pro 5G',
+      image: XiaomiRedmiNote11Pro5G
     }
   ]
 } as const;
