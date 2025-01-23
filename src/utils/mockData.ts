4d7ed8b5111838ed1139bd1d3351ca75b95672e9
uman95 <usman.aganiy@gmail.com>
2025-01-23 22:21:56 +0100

    added phone info

--- src/utils/mockData.ts
+++ src/utils/mockData.ts
@@ -1,21 +1,133 @@
 import { Phone } from '../types/phone';
 
 // Image Imports
-import GooglePixel6 from '../assets/images/GooglePixel6.webp';
-import GooglePixel7Pro from '../assets/images/GooglePixel7Pro.webp';
-import GooglePixel9Pro from '../assets/images/GooglePixel9Pro.webp';
 
-import SamsungUtraS24 from '../assets/images/Samsung Galaxy S24 Ultra.jpg';
-import SamsungS23Ultra from '../assets/images/Samsung Galaxy S24 Plus.jpg';
-import SamsungA54 from '../assets/images/Samsung Galaxy S24 Plus.jpg';
+import GooglePixel6 from '../assets/images/GooglePixel6.jpg';
+import GooglePixel6a from '../assets/images/GooglePixel6a.jpg';
+import GooglePixel6Pro from '../assets/images/GooglePixel6Pro.jpg';
+import GooglePixel7 from '../assets/images/GooglePixel7.jpg';
+import GooglePixel7a from '../assets/images/GooglePixel7a.jpg';
+import GooglePixel7Pro from '../assets/images/GooglePixel7Pro.jpg';
+import GooglePixel8 from '../assets/images/GooglePixel8.jpg';
+import GooglePixel8a from '../assets/images/Google Pixel 8a.jpg';
+import GooglePixel8Pro from '../assets/images/GooglePixel8Pro.jpg';
+import GooglePixel9 from '../assets/images/GooglePixel9.jpg';
+import GooglePixel9Pro from '../assets/images/GooglePixel9Pro.jpg';
+import GooglePixel9ProXL from '../assets/images/GooglePixel9ProXL.jpg';
+import GooglePixel9ProFold from '../assets/images/GooglePixel9ProFold.jpg';
+import GooglePixelFold from '../assets/images/GooglePixelFold.jpg';
 
-import Xiaomi14Ultra from '../assets/images/Xiaomi 14 Ultra.jpg';
-import Xiaomi14 from '../assets/images/Xiaomi Redmi Note 14 Pro Plus 5G.jpg';
-import XiaomiRedmiNote13Pro from '../assets/images/Xiaomi Redmi Note 14 Pro Plus 5G.jpg';
+// import Honor50 from '../assets/images/Honor50.webp';
+// import HonorMagic4Pro from '../assets/images/HonorMagic4Pro.webp';
 
-import OnePlus12 from '../assets/images/OnePlus 12.jpg';
-import OnePlus12R from '../assets/images/OnePlus 12R.jpg';
-import OnePlus11 from '../assets/images/OnePlus 12.jpg';
+// import MotorolaRazr40Ultra from '../assets/images/MotorolaRazr40Ultra.webp';
+
+import NothingPhone1 from '../assets/images/NothingPhone1.jpg';
+import NothingPhone2 from '../assets/images/NothingPhone2.jpg';
+
+import OnePlus10Pro from '../assets/images/OnePlus10Pro.jpg';
+import OnePlus10T from '../assets/images/OnePlus10T.jpg';
+import OnePlus12 from '../assets/images/OnePlus12.jpg';
+import OnePlus12R from '../assets/images/OnePlus12R.jpg';
+import OnePlus11 from '../assets/images/OnePlus11.jpg';
+import OnePlusOpen from '../assets/images/OnePlusOpen.jpg';
+
+import OppoFindN2Flip from '../assets/images/OppoFindN2Flip.jpg';
+import OppoFindX5 from '../assets/images/OppoFindX5.jpg';
+import OppoFindX5Lite from '../assets/images/OppoFindX5Lite.jpg';
+import OppoFindX5Pro from '../assets/images/OppoFindX5Pro.jpg';
+import OppoFindX8 from '../assets/images/OppoFindX8.jpg';
+import OppoReno8 from '../assets/images/OppoReno8.jpg';
+import OppoReno8Lite from '../assets/images/OppoReno8Lite.jpg';
+import OppoReno8Pro from '../assets/images/OppoReno8Pro.jpg';
+import OppoReno12 from '../assets/images/OppoReno12.jpg';
+import OppoReno12F from '../assets/images/OppoReno12F.jpg';
+
+
+import SonyXperia1III from '../assets/images/SonyXperia1III.jpg';
+import SonyXperia1IV from '../assets/images/SonyXperia1IV.jpg';
+import SonyXperia1V from '../assets/images/SonyXperia1V.jpg';
+import SonyXperia1VI from '../assets/images/SonyXperia1VI.jpg';
+import SonyXperia5II from '../assets/images/SonyXperia5II.jpg';
+import SonyXperia5W from '../assets/images/SonyXperia5W.jpg';
+import SonyXperia10III from '../assets/images/SonyXperia10III.jpg';
+import SonyXperia10V from '../assets/images/SonyXperia10V.jpg';
+import SonyXperia10VI from '../assets/images/SonyXperia10VI.jpg';
+import SonyXperia10W from '../assets/images/SonyXperia10W.jpg';
+import SonyXperiaProI from '../assets/images/SonyXperiaProI.jpg';
+
+
+import XiaomiRedmiA3 from '../assets/images/XiaomiRedmiA3.jpg';
+import XiaomiPocoM6 from '../assets/images/XiaomiPocoM6.jpg';
+import XiaomiPocoX5 from '../assets/images/XiaomiPocoX5.jpg';
+import XiaomiPocoX5Pro from '../assets/images/XiaomiPocoX5Pro.jpg';
+import XiaomiPocoX6Pro from '../assets/images/XiaomiPocoX6Pro.jpg';
+import XiaomiPocoX7 from '../assets/images/XiaomiPocoX7.jpg';
+import XiaomiRedmi10C from '../assets/images/XiaomiRedmi10C.jpg';
+import Xiaomi11T from '../assets/images/Xiaomi11T.jpg';
+import Xiaomi11TPro from '../assets/images/Xiaomi11TPro.jpg';
+import XiaomiRedmiNote11 from '../assets/images/XiaomiRedmiNote11.jpg';
+import XiaomiRedmiNote11Pro5G from '../assets/images/XiaomiRedmiNote11Pro5G.jpg';
+import Xiaomi12 from '../assets/images/Xiaomi12.jpg';
+import XiaomiRedmi12C from '../assets/images/XiaomiRedmi12C.jpg';
+import Xiaomi12Pro from '../assets/images/Xiaomi12Pro.jpg';
+import XiaomiRedmi13C from '../assets/images/XiaomiRedmi13C.jpg';
+import Xiaomi13T from '../assets/images/Xiaomi13T.jpg';
+import Xiaomi13TPro from '../assets/images/Xiaomi13TPro.jpg';
+import Xiaomi14 from '../assets/images/Xiaomi14.jpg';
+import XiaomiRedmi14C from '../assets/images/XiaomiRedmi14C.jpg';
+import Xiaomi14T from '../assets/images/Xiaomi14T.jpg';
+import Xiaomi14Ultra from '../assets/images/Xiaomi14Ultra.jpg';
+import Xiaomi15 from '../assets/images/Xiaomi15.jpg';
+import Xiaomi15Pro from '../assets/images/Xiaomi15Pro.jpg';
+
+
+import SamsungGalaxyA04 from '../assets/images/SamsungGalaxyA04.jpg';
+import SamsungGalaxyA06 from '../assets/images/SamsungGalaxyA06.jpg';
+import SamsungGalaxyA13 from '../assets/images/SamsungGalaxyA13.jpg';
+import SamsungGalaxyA14 from '../assets/images/SamsungGalaxyA14.jpg';
+import SamsungGalaxyA23 from '../assets/images/SamsungGalaxyA23.jpg';
+import SamsungGalaxyA34 from '../assets/images/SamsungGalaxyA34.jpg';
+import SamsungGalaxyA54 from '../assets/images/SamsungGalaxyA54.jpg';
+import SamsungGalaxyC55 from '../assets/images/SamsungGalaxyC55.jpg';
+import SamsungGalaxyF13 from '../assets/images/SamsungGalaxyF13.jpg';
+import SamsungGalaxyF55 from '../assets/images/SamsungGalaxyF55.jpg';
+import SamsungGalaxyM05 from '../assets/images/SamsungGalaxyM05.jpg';
+import SamsungGalaxyM13 from '../assets/images/SamsungGalaxyM13.jpg';
+import SamsungGalaxyM14 from '../assets/images/SamsungGalaxyM14.jpg';
+import SamsungGalaxyM15 from '../assets/images/SamsungGalaxyM15.jpg';
+import SamsungGalaxyM23 from '../assets/images/SamsungGalaxyM23.jpg';
+import SamsungGalaxyM35 from '../assets/images/SamsungGalaxyM35.jpg';
+import SamsungGalaxyM55 from '../assets/images/SamsungGalaxyM55.jpg';
+import SamsungGalaxyS20FE from '../assets/images/SamsungGalaxyS20FE.jpg';
+import SamsungGalaxyS23FE from '../assets/images/SamsungGalaxyS23FE.jpg';
+import SamsungGalaxyS23Ultra from '../assets/images/SamsungGalaxyS23Ultra.jpg';
+import SamsungGalaxyS24plus from '../assets/images/SamsungGalaxyS24Plus.jpg';
+import SamsungGalaxyS24 from '../assets/images/SamsungGalaxyS24.jpg';
+import SamsungGalaxyS24FE from '../assets/images/SamsungGalaxyS24FE.jpg';
+import SamsungGalaxyS24Ultra from '../assets/images/SamsungGalaxyS24Ultra.jpg';
+import SamsungGalaxyZFlip4 from '../assets/images/SamsungGalaxyZFlip4.jpg';
+import SamsungGalaxyZFlip5 from '../assets/images/SamsungGalaxyZFlip5.jpg';
+import SamsungGalaxyZFold4 from '../assets/images/SamsungGalaxyZFold4.jpg';
+import SamsungGalaxyZFold5 from '../assets/images/SamsungGalaxyZFold5.jpg';
+import SamsungGalaxyZFlip6 from '../assets/images/SamsungGalaxyZFlip6.jpg';
+import SamsungGalaxyZFold6 from '../assets/images/SamsungGalaxyZFold6.jpg';
+
+// import GooglePixel6 from '../assets/images/GooglePixel6.webp';
+// import GooglePixel7Pro from '../assets/images/GooglePixel7Pro.webp';
+// import GooglePixel9Pro from '../assets/images/GooglePixel9Pro.webp';
+
+// import SamsungUtraS24 from '../assets/images/Samsung Galaxy S24 Ultra.jpg';
+// import SamsungS23Ultra from '../assets/images/Samsung Galaxy S24 Plus.jpg';
+// import SamsungA54 from '../assets/images/Samsung Galaxy S24 Plus.jpg';
+
+// import Xiaomi14Ultra from '../assets/images/Xiaomi 14 Ultra.jpg';
+// import Xiaomi14 from '../assets/images/Xiaomi Redmi Note 14 Pro Plus 5G.jpg';
+// import XiaomiRedmiNote13Pro from '../assets/images/Xiaomi Redmi Note 14 Pro Plus 5G.jpg';
+
+// import OnePlus12 from '../assets/images/OnePlus 12.jpg';
+// import OnePlus12R from '../assets/images/OnePlus 12R.jpg';
+// import OnePlus11 from '../assets/images/OnePlus 12.jpg';
 
 import MotorolaEdge40Pro from '../assets/images/Motorola Edge 40 Pro.jpg';
 import MotorolaEdge40 from '../assets/images/Motorola Edge 40 Pro.jpg';
@@ -35,8 +147,178 @@ export const MOCK_PHONES: Phone[] = [
     imageUrl: GooglePixel6,
     isNew: false,
     dimensions: 'Standard',
-    storage: '128GB',
-    ram: '8GB',
+    storage: ['128GB'],
+    ram: ['8GB'],
+    screenResolution: 'FHD+',
+    releaseYear: 2021
+  },
+  {
+    id: 'pixel-6-002',
+    name: 'Google Pixel 6a',
+    brand: 'Google',
+    price: 599,
+    primaryCamera: '50MP',
+    features: ['5G', 'Wireless charging', 'Fast charging'],
+    batteryLife: 'Up to 24 hours',
+    screenSize: '6" - 6.5"',
+    imageUrl: GooglePixel6a,
+    isNew: false,
+    dimensions: 'Standard',
+    storage: ['128GB'],
+    ram: ['8GB'],
+    screenResolution: 'FHD+',
+    releaseYear: 2021
+  },
+  {
+    id: 'pixel-6-003',
+    name: 'Google Pixel 6 Pro',
+    brand: 'Google',
+    price: 599,
+    primaryCamera: '50MP',
+    features: ['5G', 'Wireless charging', 'Fast charging'],
+    batteryLife: 'Up to 24 hours',
+    screenSize: '6" - 6.5"',
+    imageUrl: GooglePixel6Pro,
+    isNew: false,
+    dimensions: 'Standard',
+    storage: ['128GB'],
+    ram: ['8GB'],
+    screenResolution: 'FHD+',
+    releaseYear: 2021
+  },
+  {
+    id: 'pixel-6-001',
+    name: 'Google Pixel 6',
+    brand: 'Google',
+    price: 599,
+    primaryCamera: '50MP',
+    features: ['5G', 'Wireless charging', 'Fast charging'],
+    batteryLife: 'Up to 24 hours',
+    screenSize: '6" - 6.5"',
+    imageUrl: GooglePixel6,
+    isNew: false,
+    dimensions: 'Standard',
+    storage: ['128GB'],
+    ram: ['8GB'],
+    screenResolution: 'FHD+',
+    releaseYear: 2021
+  },
+  {
+    id: 'pixel-6-001',
+    name: 'Google Pixel 6',
+    brand: 'Google',
+    price: 599,
+    primaryCamera: '50MP',
+    features: ['5G', 'Wireless charging', 'Fast charging'],
+    batteryLife: 'Up to 24 hours',
+    screenSize: '6" - 6.5"',
+    imageUrl: GooglePixel6,
+    isNew: false,
+    dimensions: 'Standard',
+    storage: ['128GB'],
+    ram: ['8GB'],
+    screenResolution: 'FHD+',
+    releaseYear: 2021
+  },
+  {
+    id: 'pixel-6-001',
+    name: 'Google Pixel 6',
+    brand: 'Google',
+    price: 599,
+    primaryCamera: '50MP',
+    features: ['5G', 'Wireless charging', 'Fast charging'],
+    batteryLife: 'Up to 24 hours',
+    screenSize: '6" - 6.5"',
+    imageUrl: GooglePixel6,
+    isNew: false,
+    dimensions: 'Standard',
+    storage: ['128GB'],
+    ram: ['8GB'],
+    screenResolution: 'FHD+',
+    releaseYear: 2021
+  },
+  {
+    id: 'pixel-6-001',
+    name: 'Google Pixel 6',
+    brand: 'Google',
+    price: 599,
+    primaryCamera: '50MP',
+    features: ['5G', 'Wireless charging', 'Fast charging'],
+    batteryLife: 'Up to 24 hours',
+    screenSize: '6" - 6.5"',
+    imageUrl: GooglePixel6,
+    isNew: false,
+    dimensions: 'Standard',
+    storage: ['128GB'],
+    ram: ['8GB'],
+    screenResolution: 'FHD+',
+    releaseYear: 2021
+  },
+  {
+    id: 'pixel-6-001',
+    name: 'Google Pixel 6',
+    brand: 'Google',
+    price: 599,
+    primaryCamera: '50MP',
+    features: ['5G', 'Wireless charging', 'Fast charging'],
+    batteryLife: 'Up to 24 hours',
+    screenSize: '6" - 6.5"',
+    imageUrl: GooglePixel6,
+    isNew: false,
+    dimensions: 'Standard',
+    storage: ['128GB'],
+    ram: ['8GB'],
+    screenResolution: 'FHD+',
+    releaseYear: 2021
+  },
+  {
+    id: 'pixel-6-001',
+    name: 'Google Pixel 6',
+    brand: 'Google',
+    price: 599,
+    primaryCamera: '50MP',
+    features: ['5G', 'Wireless charging', 'Fast charging'],
+    batteryLife: 'Up to 24 hours',
+    screenSize: '6" - 6.5"',
+    imageUrl: GooglePixel6,
+    isNew: false,
+    dimensions: 'Standard',
+    storage: ['128GB'],
+    ram: ['8GB'],
+    screenResolution: 'FHD+',
+    releaseYear: 2021
+  },
+  {
+    id: 'pixel-6-001',
+    name: 'Google Pixel 6',
+    brand: 'Google',
+    price: 599,
+    primaryCamera: '50MP',
+    features: ['5G', 'Wireless charging', 'Fast charging'],
+    batteryLife: 'Up to 24 hours',
+    screenSize: '6" - 6.5"',
+    imageUrl: GooglePixel6,
+    isNew: false,
+    dimensions: 'Standard',
+    storage: ['128GB'],
+    ram: ['8GB'],
+    screenResolution: 'FHD+',
+    releaseYear: 2021
+  },
+  {
+    id: 'pixel-6-001',
+    name: 'Google Pixel 6',
+    brand: 'Google',
+    price: 599,
+    primaryCamera: '50MP',
+    features: ['5G', 'Wireless charging', 'Fast charging'],
+    batteryLife: 'Up to 24 hours',
+    screenSize: '6" - 6.5"',
+    imageUrl: GooglePixel6,
+    isNew: false,
+    dimensions: 'Standard',
+    storage: ['128GB'],
+    ram: ['8GB'],
     screenResolution: 'FHD+',
     releaseYear: 2021
   },
